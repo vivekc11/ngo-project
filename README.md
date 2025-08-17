d6118d7604fd0e6def80f32e72e71b443af1b9d7
Vivek Chatani <vivek.chatani2006@gmail.com>
2025-08-17 12:55:13 +0530

    commiting - ngo-v1 working scrapy

---
+++ README.md
@@ -0,0 +1,4 @@
+Ngo - v1
+
+lots of files are redundent, useless, might need rearagning
+better logs 
--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from app import app # Import your Flask app instance
 
 if __name__ == "__main__":
     # Runs the Flask development server
-    app.run(host="0.0.0.0", port=5050, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
